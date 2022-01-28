f69f02655d17212831a88b5b460c8ccb06dbe404
Lim Hyun <hyunzion@gmail.com>
2022-01-28 22:08:12 +0900

    Create image_crop.py

---
+++ src/python/image_crop.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+def image_cropping(name, rectangle):
+    image_path = "../html/image/meal.jpg"
+    (left, upper, right, lower) = rectangle
+
+    with Image.open(image_path) as im:
+        im_crop = im.crop((left, upper, right, lower))
+        im_crop.save("../html/image/" + name + ".jpg")
+
+
+image_cropping("week", (360, 480, 2220, 1090))
+
+image_cropping("mon", (360, 480, 730, 1090))
+image_cropping("tus", (730, 480, 1130, 1090))
+image_cropping("wed", (1135, 480, 1470, 1090))
+image_cropping("thu", (1475, 480, 1845, 1090))
+image_cropping("fri", (1845, 480, 2220, 1090))
+
+image_cropping("division", (110, 480, 360, 1090))
+image_cropping("date", (360, 165, 2220, 265))
