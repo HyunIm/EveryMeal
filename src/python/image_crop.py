1f89704924493365810b785ade0e1c269b3a92f4
Lim Hyun <hyunzion@gmail.com>
2022-02-01 11:48:31 +0900

    Fix Tuesday Image
    tus -> tue

--- src/python/image_crop.py
+++ src/python/image_crop.py
@@ -16,7 +16,7 @@ def execute_image_crop():
     image_cropping("week", (360, 480, 2220, 1090))
 
     image_cropping("mon", (360, 480, 730, 1090))
-    image_cropping("tus", (730, 480, 1130, 1090))
+    image_cropping("tue", (730, 480, 1130, 1090))
     image_cropping("wed", (1135, 480, 1470, 1090))
     image_cropping("thu", (1475, 480, 1845, 1090))
     image_cropping("fri", (1845, 480, 2220, 1090))
