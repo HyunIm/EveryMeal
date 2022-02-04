c431c1d28628aa6ecb368bdef80030610cbafe3e
Lim Hyun <hyunzion@gmail.com>
2022-02-04 20:50:37 +0900

    Add Crop Data (Week 6)

--- src/python/pdf_to_image.py
+++ src/python/pdf_to_image.py
@@ -5,4 +5,4 @@ def execute_pdf_to_image():
     print("execute_pdf_to_image...")
     
     images = convert_from_path("data/meal.pdf")
-    images[0].save("../html/image/meal.jpg", "JPEG")
+    images[0].save("../html/image/meal.jpg")
